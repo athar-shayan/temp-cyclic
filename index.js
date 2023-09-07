3459d0021294e8b978111e92302a41615b3de755
athar.shayan <athar.shayansolutions@gmail.com>
2023-09-07 15:48:57 +0500

    express server

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http');
- 
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
 const port = 3000;
- 
-const server = http.createServer((req, res) => {
-  res.end('Hello World in temp');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
- 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
